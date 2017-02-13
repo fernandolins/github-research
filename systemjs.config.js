774b22969c2e4062f208034ab825d2f345ff8acc
Fernando Lins <fernando@labcodes.com.br>
2017-02-13 18:36:34 -0300

    Add systemjs.config.js: angular2 config.

---
+++ systemjs.config.js
@@ -0,0 +1,14 @@
+/**
+ * System configuration for Angular samples
+ * Adjust as necessary for your application needs.
+ */
+(function (global) {
+  System.config({
+    packages: {
+      app: {
+        format: 'register',
+        defaultExtension: 'js'
+      }
+    }
+  });
+})(this);
